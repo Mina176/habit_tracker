eec6347ff1f32f37cb63fe1cb08a644e8b89b4bc
Mina Ayman <mtrmena176@gmail.com>
2025-12-20 18:48:52 +0200

    task completion ring

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/ui/theming/app_theme.dart';
+import 'package:habit_tracker/ui/widgets/task_completion_ring.dart';
 
 class homePage extends StatelessWidget {
   const homePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppTheme.of(context).primary);
+    return Scaffold(
+      backgroundColor: AppTheme.of(context).primary,
+      body: Center(
+        child: SizedBox(
+          width: 120,
+          child: taskCompletionRing(),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/ui/widgets/task_completion_ring.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/ui/theming/app_theme.dart';
+
+class taskCompletionRing extends StatelessWidget {
+  const taskCompletionRing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: CustomPaint(
+        painter: RingPainter(
+          progress: 0.5,
+          taskNotCompletedColor: AppTheme.of(context).taskRing,
+          taskCompletedColor: AppTheme.of(context).accent,
+        ),
+      ),
+    );
+  }
+}
+
+class RingPainter extends CustomPainter {
+  RingPainter({
+    super.repaint,
+    required this.progress,
+    required this.taskNotCompletedColor,
+    required this.taskCompletedColor,
+  });
+  final double progress;
+  final Color taskNotCompletedColor;
+  final Color taskCompletedColor;
+  @override
+  void paint(Canvas canvas, Size size) {
+    final strokeWidth = size.width / 10.0;
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width - strokeWidth / 2;
+
+    final backgroundPaint = Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = strokeWidth
+      ..color = taskNotCompletedColor
+      ..style = PaintingStyle.stroke;
+    canvas.drawCircle(center, radius, backgroundPaint);
+
+    final foregroundPaint = Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = strokeWidth
+      ..color = taskCompletedColor
+      ..style = PaintingStyle.stroke;
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -pi / 2,
+      progress * pi * 2,
+      false,
+      foregroundPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant RingPainter oldDelegate) =>
+      oldDelegate.progress != progress;
+}
