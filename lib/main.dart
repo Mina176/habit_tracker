0a1d4a04c5dc653c05802e19210ae72f32c81673
Mina Ayman <mtrmena176@gmail.com>
2025-12-20 02:03:01 +0200

    assets - colors - textStyles - themeData

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/constants/app_assets.dart';
+import 'package:habit_tracker/constants/app_colors.dart';
+import 'package:habit_tracker/ui/home/home_page.dart';
+import 'package:habit_tracker/ui/theming/app_theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppAssets.preloadSVGs();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'HelveticaNeue'),
+
+      home: AppTheme(
+        data: AppThemeData.defaultWithSwatch(AppColors.red),
+        child: homePage(),
+      ),
+    );
+  }
+}
