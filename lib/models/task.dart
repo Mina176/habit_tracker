f3a92e0421ee00d0773a7f38baa0985efd390a07
Mina Ayman <mtrmena176@gmail.com>
2025-12-23 15:06:51 +0200

    create task model

---
+++ lib/models/task.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+
+
+class Task  {
+  final int id;
+  final String icon;
+  final String iconName;
+
+  Task({required this.id, required this.icon, required this.iconName});
+}
